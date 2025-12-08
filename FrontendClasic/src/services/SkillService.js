dc678dbb5e5e37ff6e7dd7abec4671549ec896f8
WakanTanka12 <samuelitorochav536@gmail.com>
2025-12-08 18:25:28 -0400

    XD

--- FrontendClasic/src/services/SkillService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api.js"
-
-const API_URL = "http://localhost:9090/api/skills";
-
-export const getAllSkills = () => api.get(API_URL);
-export const getSkillById = (id) => api.get(`${API_URL}/${id}`);
-export const createSkill = (skill) => api.post(API_URL, skill);
-export const updateSkill = (id, skill) => api.put(`${API_URL}/${id}`, skill);
-export const deleteSkill = (id) => api.delete(`${API_URL}/${id}`);
--- FrontendClasic/src/services/departmentService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api.js"
-
-const API_URL = "http://localhost:9090/api/departments";
-
-export const getAllDepartments = () => api.get(API_URL);
-export const getDepartmentById = (id) => api.get(`${API_URL}/${id}`);
-export const createDepartment = (department) => api.post(API_URL, department);
-export const updateDepartment = (id, department) => api.put(`${API_URL}/${id}`, department);
-export const deleteDepartment = (id) => api.delete(`${API_URL}/${id}`);
