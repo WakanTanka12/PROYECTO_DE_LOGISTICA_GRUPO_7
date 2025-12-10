2b15689251325a7751a275c20c23648e7a834659
Sergio3457 <a2024117171@estudiantes.upsa.edu.bo>
2025-12-09 20:45:48 -0400

    delivery fix

--- ReactWeb/src/services/DeliveryService.js
+++ ReactWeb/src/services/DeliveryService.js
@@ -4,6 +4,11 @@ const DELIVERY_URL = "http://localhost:8080/api/deliveries";
 
 export const getAllDeliveries = () => axios.get(DELIVERY_URL);
 export const getDeliveryById = (id) => axios.get(`${DELIVERY_URL}/${id}`);
-export const createDelivery = (delivery) => axios.post(DELIVERY_URL, delivery);
+export const createDelivery = (delivery) =>
+    axios.post(
+        `http://localhost:8080/api/drivers/${delivery.driverId}/deliveries`,
+        delivery
+    );
+
 export const updateDelivery = (id, delivery) => axios.put(`${DELIVERY_URL}/${id}`, delivery);
 export const deleteDelivery = (id) => axios.delete(`${DELIVERY_URL}/${id}`);
